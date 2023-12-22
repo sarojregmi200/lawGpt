0cae1f0f712e75b878df1fa692be6e421f47c6e3
Saroj Regmi <sarojregmi.official@gmail.com>
2023-12-22 08:11:14 +0545

    Complete the cookie based auth

--- web/src/components/chat/message/UserMessage.tsx
+++ web/src/components/chat/message/UserMessage.tsx
@@ -4,36 +4,36 @@ import { useUserContext } from "@/context/UserContext";
 import Image from "next/image";
 import moment from "moment";
 type UserMessageProps = {
-  message: string;
-  time: string;
+    message: string;
+    time: string;
 };
 const UserMessage = ({
-  data: { message, time },
+    data: { message, time },
 }: {
-  data: UserMessageProps;
+    data: UserMessageProps;
 }) => {
-  const { user } = useUserContext();
-  return (
-    <div className=" my-[24px]">
-      <div className="relative w-full h-max">
-        <div className="flex justify-between w-full z-10">
-          <Image
-            className="rounded-[5px]"
-            height={"50"}
-            width={"50"}
-            alt="SL"
-            src={user.profile}
-          />
-          <span className="text-xs text-gray-400 mt-auto mb-[20px]">
-            {moment.relativeTimeThreshold("ss") && moment(time).fromNow()}
-          </span>
+    const { user } = useUserContext();
+    return (
+        <div className=" my-[24px]">
+            <div className="relative w-full h-max">
+                <div className="flex justify-between w-full z-10">
+                    <Image
+                        className="rounded-[5px]"
+                        height={"50"}
+                        width={"50"}
+                        alt="SL"
+                        src={user.picture}
+                    />
+                    <span className="text-xs text-gray-400 mt-auto mb-[20px]">
+                        {moment.relativeTimeThreshold("ss") && moment(time).fromNow()}
+                    </span>
+                </div>
+                <div className="bg-d-active-chat rounded-[5px] p-[24px]  w-[calc(100%-15px)] mt-[-15px] ml-auto z-0">
+                    <span>{message}</span>
+                </div>
+            </div>
         </div>
-        <div className="bg-d-active-chat rounded-[5px] p-[24px]  w-[calc(100%-15px)] mt-[-15px] ml-auto z-0">
-          <span>{message}</span>
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default UserMessage;
