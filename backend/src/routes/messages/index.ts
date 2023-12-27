6e213c7bc4b72480e86cce4606945772d787132a
Saroj Regmi <sarojregmi.official@gmail.com>
2023-12-28 00:15:12 +0545

    Pagination setup done

--- backend/src/routes/messages/index.ts
+++ backend/src/routes/messages/index.ts
@@ -1,11 +1,15 @@
 import { validateJwt } from "$/utils/jwtToken";
 import { Router } from "express";
-import { addMessageToMessageRoom, getAllMessages } from "./messageControllers";
+import {
+    addMessageToMessageRoom,
+    getAllMessages,
+    getPaginatedMessage
+} from "./messageControllers";
 
 const messageRouter = Router();
 
 messageRouter.get("/:roomId", validateJwt, getAllMessages)
-messageRouter.get("/:roomId/count", validateJwt, getAllMessages)
+messageRouter.get("/:roomId/paginated", validateJwt, getPaginatedMessage)
 
 messageRouter.post("/:roomId", validateJwt, addMessageToMessageRoom)
 
--- backend/src/routes/messages/messageControllers.ts
+++ backend/src/routes/messages/messageControllers.ts
@@ -27,7 +27,6 @@ export const getAllMessages = async (req: Request, res: Response) => {
             execute<TmessageRoomsResponse[]>(`SELECT tableName FROM LAW_GPT_MESSAGE_ROOMS WHERE _id = ? AND _user_id = ?`,
                 [roomId, userData._id])
 
-
         if (roomEntry.length < 1)
             return res.status(404).json({ msg: "No tables found to get the messages" })
 
@@ -97,4 +96,55 @@ export const addMessageToMessageRoom = async (req: Request, res: Response) => {
         })
     }
 }
+
+export const getPaginatedMessage = async (req: Request, res: Response) => {
+    try {
+        const userData = res.locals.userData
+        if (!userData || !userData._id) throw new Error("No user found")
+
+        let { roomId } = req.params;
+        type TpaginatedOptions = {
+            fromId: string,
+            count: number,
+            direction: "forward" | "backward"
+        }
+        const paginationOptions: TpaginatedOptions = req.body.paginationOptions;
+
+        if (!paginationOptions.fromId
+            || !paginationOptions.count
+            || !paginationOptions.direction) {
+            return res.status(400).json({ msg: "Invalid paginationOptions." })
+        }
+
+        if (!roomId)
+            return res.status(400).json({ msg: "No room Id provided" })
+
+        const [roomEntry] = await dbConnection.
+            execute<TmessageRoomsResponse[]>(`SELECT tableName FROM LAW_GPT_MESSAGE_ROOMS WHERE _id = ? AND _user_id = ?`,
+                [roomId, userData._id])
+
+        if (roomEntry.length < 1)
+            return res.status(404).json({ msg: "No message room with the given id found" })
+
+        const messageTableName = roomEntry[0]?.tableName;
+
+        type TmessageResponse = Tmessage & RowDataPacket
+
+        const [messages] = await dbConnection.
+            execute<TmessageResponse[]>(`SELECT * FROM ${messageTableName} WHERE _user_id = ?`,
+                [userData._id])
+
+        return res.status(200).json({
+            msg: `Loaded ${messages.length} messages`,
+            messages: messages
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            msg: "Internal server error",
+            error: error
+        })
+    }
+}
+
 
