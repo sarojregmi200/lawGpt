12e59130ab1c897ae843c34319530294338d786c
Saroj Regmi <sarojregmi.official@gmail.com>
2023-12-27 10:08:53 +0545

    Create message route complete

--- backend/src/routes/messages/messageControllers.ts
+++ backend/src/routes/messages/messageControllers.ts
@@ -1,6 +1,14 @@
 import dbConnection from "$/utils/db"
+import { randomUUID } from "crypto";
 import { Request, Response } from "express"
 import { RowDataPacket } from "mysql2"
+
+interface Tmessage {
+    _id: string;
+    _user_id: string;
+    message: string;
+    createdAt?: Date;
+}
 
 export const getAllMessages = async (req: Request, res: Response) => {
     try {
@@ -8,6 +16,8 @@ export const getAllMessages = async (req: Request, res: Response) => {
         if (!userData || !userData._id) throw new Error("No user found")
 
         const { roomId } = req.params;
+        if (!roomId)
+            return res.status(400).json({ msg: "No room Id provided" })
 
         console.log(roomId)
         interface TmessageRoomsResponse extends RowDataPacket {
@@ -24,12 +34,7 @@ export const getAllMessages = async (req: Request, res: Response) => {
         if (!messageTableName)
             return res.status(404).json({ msg: "No tables found to get the messages" })
 
-        interface TmessageResponse extends RowDataPacket {
-            _id: string;
-            _user_id: string;
-            message: string;
-            createdAt: Date;
-        }
+        type TmessageResponse = Tmessage & RowDataPacket
 
         const [messages] = await dbConnection.
             execute<TmessageResponse[]>(`SELECT * FROM ${messageTableName} WHERE _user_id = ?`,
@@ -50,4 +55,52 @@ export const getAllMessages = async (req: Request, res: Response) => {
 }
 
 
+export const addMessageToMessageRoom = async (req: Request, res: Response) => {
+    try {
+        const userData = res.locals.userData
+        if (!userData || !userData._id) throw new Error("No user found")
+
+        const { roomId } = req.params;
+
+        interface TmessageRoomsResponse extends RowDataPacket {
+            tableName: string
+        }
+
+        if (!roomId)
+            return res.status(400).json({ msg: "No room Id provided" })
+
+        const [roomEntry] = await dbConnection.
+            execute<TmessageRoomsResponse[]>(`SELECT tableName FROM LAW_GPT_MESSAGE_ROOMS WHERE _id = ? AND _user_id = ?`,
+                [roomId, userData._id])
+
+        const messageTableName = roomEntry[0]?.tableName;
+        if (!messageTableName)
+            return res.status(404).json({ msg: "No tables found to get the messages" })
+
+        const messageFromReq: string = req.body.message;
+        if (!messageFromReq.trim())
+            res.status(400).json({ msg: "Message cannot be empty string" })
+
+        const messageToInsert: Tmessage = {
+            _id: randomUUID(),
+            _user_id: userData._id,
+            message: messageFromReq
+        };
+
+        await dbConnection.
+            execute(`INSERT INTO  ${messageTableName} (_id, _user_id, message) VALUES (?, ?, ?)`,
+                [...Object.values(messageToInsert)])
+
+        return res.status(200).json({
+            msg: "Message Inserted successfully"
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            msg: "Internal server error",
+            error: error
+        })
+    }
+
+}
 
