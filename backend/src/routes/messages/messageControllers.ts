74868114ac57b0dbfeb3ab8e83fef0aa42aec755
Saroj Regmi <sarojregmi.official@gmail.com>
2023-12-29 09:22:52 +0545

    Proper error handeling of the pagination options.

--- backend/src/routes/messages/messageControllers.ts
+++ backend/src/routes/messages/messageControllers.ts
@@ -113,11 +113,18 @@ export const getPaginatedMessage = async (req: Request, res: Response) => {
         }
         const paginationOptions: TpaginatedOptions = req.body.paginationOptions;
 
-        if (!paginationOptions.fromId
-            || !paginationOptions.count
-            || !paginationOptions.direction) {
+        console.log(paginationOptions)
+
+        if (!paginationOptions?.fromId
+            || !paginationOptions?.count
+            || !paginationOptions?.direction
+            || !(
+                paginationOptions?.direction === "forward"
+                || paginationOptions?.direction === "backward"
+            )
+        )
             return res.status(400).json({ msg: "Invalid paginationOptions." })
-        }
+
 
         if (!roomId)
             return res.status(400).json({ msg: "No room Id provided" })
@@ -129,17 +136,25 @@ export const getPaginatedMessage = async (req: Request, res: Response) => {
         if (roomEntry.length < 1)
             return res.status(404).json({ msg: "No message room with the given id found" })
 
-        const messageTableName = roomEntry[0]?.tableName;
+        const tableName = roomEntry[0].tableName;
 
-        type TmessageResponse = Tmessage & RowDataPacket
-
-        const [messages] = await dbConnection.
-            execute<TmessageResponse[]>(`SELECT * FROM ${messageTableName} WHERE _user_id = ?`,
-                [userData._id])
+        const paginatedSql = `
+                SELECT * FROM ? WHERE _id ${paginationOptions.direction === "forward" ? ">=" : "<="} ?,
+                SELECT COUNT(*) FROM ? WHERE _id > ? AS messages_below,
+                SELECT COUNT(*) FROM ? WHERE _id < ? AS messages_above
+        `
+        const paginatedResponse = await dbConnection.
+            execute(paginatedSql,
+                [
+                    tableName, paginationOptions.fromId,
+                    tableName, paginationOptions.fromId,
+                    tableName, paginationOptions.fromId
+                ]
+            )
+        console.log(paginatedResponse)
 
         return res.status(200).json({
-            msg: `Loaded ${messages.length} messages`,
-            messages: messages
+            msg: `Fetched ${paginationOptions.count} message from ${paginationOptions.fromId} id`,
         })
     } catch (error) {
         console.log(error)
