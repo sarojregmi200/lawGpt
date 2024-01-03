609706f68ef10b33468ff99a580f25496789ec71
Saroj Regmi <sarojregmi.official@gmail.com>
2024-01-03 23:42:14 +0545

    Feat: breaking pdfs.

--- ai/src/index.ts
+++ ai/src/index.ts
@@ -1,1 +1,15 @@
-// write a file parser and then parse each line
+import * as fileEntries from "./data/fileEntries.json"
+
+type TfileEntries = {
+    fileName: string,
+    country: string,
+    filePath: string
+}[]
+
+try {
+    let referenceFile: TfileEntries = fileEntries;
+    console.log(referenceFile[0])
+
+} catch (error) {
+    console.error("unexpected error occured", error)
+}
