e4c9461f80a4db3742dc994ac8f5612f99f0b929
Saroj Regmi <sarojregmi.official@gmail.com>
2024-01-04 23:15:38 +0545

    Added transfromers.js for embedding generation

--- ai/src/index.ts
+++ ai/src/index.ts
@@ -1,4 +1,6 @@
+import { config } from "dotenv";
 import * as fileEntries from "./data/fileEntries.json"
+import { PipelineType, pipeline } from "@xenova/transformers";
 
 type TfileEntries = {
     fileName: string,
@@ -6,10 +8,34 @@ type TfileEntries = {
     filePath: string
 }[]
 
-try {
-    let referenceFile: TfileEntries = fileEntries;
-    console.log(referenceFile[0])
+async function main() {
+    try {
+        // configuring the dotenv
+        config();
 
-} catch (error) {
-    console.error("unexpected error occured", error)
+        const generateEmbedding = await pipeline(
+            "feature-extraction",
+            "xenova/all-MiniLM-L6-v2"
+        )
+
+        const text = "some random text"
+        const anotherText = "some random text"
+
+        const emb1 = await generateEmbedding(text, {
+            pooling: 'mean',
+            normalize: true
+        })
+
+        const emb2 = await generateEmbedding(anotherText, {
+            pooling: 'mean',
+            normalize: true
+        })
+        console.log(emb1, emb2)
+
+        let referenceFile: TfileEntries = fileEntries;
+    } catch (error) {
+        console.error("unexpected error occured", error)
+    }
 }
+
+main()
